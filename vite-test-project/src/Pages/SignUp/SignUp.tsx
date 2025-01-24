7dd88619b3a9af483b6afd5ddd304352a02ec5bb
Moontoast435 <seanmalone1993@gmail.com>
2025-01-24 08:21:13 +0000

    add elements to sign up form and styling

--- vite-test-project/src/Pages/SignUp/SignUp.tsx
+++ vite-test-project/src/Pages/SignUp/SignUp.tsx
@@ -1,5 +1,33 @@
 import React from "react";
+import "./styles.css";
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <div className="signup-container">
+        <form>
+          <fieldset className="signUpFieldset">
+            <label htmlFor="signupUsernameInput">Username: </label>
+            <input
+              id="signupUsernameInput"
+              type="text"
+              placeholder="Enter username here"
+            ></input>
+            <label htmlFor="signupPasswordInput">Password: </label>
+            <input
+              id="signupPasswordInput"
+              type="password"
+              placeholder="Enter password here"
+            ></input>
+            <label htmlFor="signupPasswordInput">Confirm Password: </label>
+            <input
+              id="signupConfirmPasswordInput"
+              type="password"
+              placeholder="Enter password confirmation here"
+            ></input>
+          </fieldset>
+        </form>
+      </div>
+    </>
+  );
 }
