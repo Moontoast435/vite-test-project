74b1eb20c98d90752568c6390cae6d384b150de5
Moontoast435 <seanmalone1993@gmail.com>
2025-03-07 21:53:34 +0000

    start 0auth implementation

---
+++ vite-test-project/Auth0Config.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: "dev-ikudfe8pxkk7cvke.us.auth0.com",
+  clientID: "9sCphdL4DR9113nlZEsbswiJQmuHtFQj",
+  redirectUri: "http://localhost:3000",
+  audience: "https://onlinebookstore.mycompany.com",
+};
